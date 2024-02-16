e5351f1b483a88979c1b996506d302a5d276f32e
Bos, Pierry <pierry.bos@gmail.com>
2024-02-16 08:05:48 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,14 @@ import "react-time-picker/dist/TimePicker.css";
 import "react-clock/dist/Clock.css";
 import moment from "moment";
 
-export default function QrCode() {
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+};
+
+export default function Page() {
   const { Canvas } = useQRCode();
   const searchParams = useSearchParams();
 
