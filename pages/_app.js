9bfece146939e19a93929b982214d23b86041b59
Bos, Pierry <pierry.bos@gmail.com>
2024-02-16 08:05:10 -0300

    Atualizar o _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,4 @@
 import "../styles/global.css";
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false
-};
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
