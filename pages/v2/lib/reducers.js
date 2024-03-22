e6a56a36aee682ee76a93c54f3545ddcf91c6d8f
Bos, Pierry <pierry.bos@gmail.com>
2024-03-22 16:39:04 +0000

    removendo versão obsoleta

--- pages/v2/lib/reducers.js
+++
@@ -1,9 +0,0 @@
-// reducers.js
-import { combineReducers } from "redux";
-import { reducer } from "./appSlice";
-
-const rootReducer = combineReducers({
-  app: reducer,
-});
-
-export default rootReducer;
